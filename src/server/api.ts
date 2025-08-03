5561c25516a9a2d84419d8f3804a7790a8a854b0
Nipodemos <nipodemos13@gmail.com>
2025-08-03 19:23:47 -0300

    incializar tabela usuários com um usuário admin

--- src/server/api.ts
+++ src/server/api.ts
@@ -1,10 +1,26 @@
 import { remultApi } from "remult/remult-sveltekit";
-import { Usuario } from "../routes/sistema/usuarios/Usuario";
+import { Usuario } from "../routes/app/usuarios/Usuario.model";
 import { Funcao } from "$lib/enums/Funcao";
+import bcrypt from "bcrypt";
 
 export const api = remultApi({
   admin: true,
   entities: [Usuario],
+  initApi: async (remult) => {
+    const repoUsuario = remult.repo(Usuario);
+
+    const usuarios = await repoUsuario.find({ limit: 1 });
+    if (usuarios.length === 0) {
+      // Cria um usuário padrão se não existir nenhum
+      const usuario = new Usuario();
+      usuario.nome = "Administrador";
+      usuario.login = "admin";
+      usuario.senha = await bcrypt.hash("admin", 10); // Senha criptografada
+      usuario.nivelPermissao = Funcao.Master;
+      await repoUsuario.insert(usuario);
+    }
+  },
+
   getUser: async (event) => {
     if (!event.locals.usuario) {
       return undefined;
