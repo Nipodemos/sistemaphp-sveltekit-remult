5f658c1bb085dd15f9f5b2cf1dbf6b29c5256011
Nipodemos <nipodemos13@gmail.com>
2025-08-03 19:25:27 -0300

    a pagina index não existe, então redireciono ou pra login ou para app

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,6 +1,16 @@
-import { remult } from "remult";
+import { redirect } from "@sveltejs/kit";
 import type { LayoutServerLoad } from "./$types";
 
-export const load = (async () => {
-  return { user: remult.user };
+export const load = (async ({ locals, url }) => {
+  // O hook (hooks.server.ts) já tentou popular `locals.usuario`.
+  // Se o usuário não estiver logado, `locals.usuario` será `null`.
+  console.log("locals.usuario :>> ", locals.usuario?.nome);
+  if (!locals.usuario) {
+    // Lança um redirecionamento para a página de login.
+    // Incluímos `from` na URL para que possamos redirecionar de volta após o login.
+    throw redirect(303, `/login?redirectTo=${url.pathname}`);
+  } else if (url.pathname === "/") {
+    throw redirect(303, `/app`);
+  }
+  return { usuario: locals.usuario };
 }) satisfies LayoutServerLoad;
