17c2cc762f5310eef048508b5bd6afa18920d4ad
Nipodemos <nipodemos13@gmail.com>
2025-08-06 00:32:05 -0300

    função agora é uma lista de string

--- src/lib/enums/Funcao.ts
+++ src/lib/enums/Funcao.ts
@@ -1,8 +1,7 @@
 export enum Funcao {
-  Master = 100,
-  Diretor = 200,
-  Supervisor = 300,
-  Gerente = 400,
-  Vendedor = 500,
-  Caixa = 500
+  Master = "master",
+  Diretor = "diretor",
+  Supervisor = "supervisor",
+  Gerente = "gerente",
+  Vendedor = "vendedor",
 }
