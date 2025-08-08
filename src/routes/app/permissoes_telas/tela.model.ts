d7ffc75fa97d9ecafef69979b9483002091e9c7b
Nipodemos <nipodemos13@gmail.com>
2025-08-07 23:30:19 -0300

    na nova forma de controlar permissoes isso não é necessário

--- src/routes/app/permissoes_telas/tela.model.ts
+++ src/routes/app/permissoes_telas/tela.model.ts
@@ -17,14 +17,6 @@ export class Tela {
   @Fields.integer()
   ordem = 0;
 
-  @Fields.object<Tela, string[]>({
-    valueConverter: {
-      toDb: (x) => (x ? x.join(",") : undefined),
-      fromDb: (x) => (x ? x.split(",") : undefined),
-    },
-  })
-  permissoesPossiveis: string[] = [];
-
   @Fields.createdAt()
   criadoEm?: Date;
 
