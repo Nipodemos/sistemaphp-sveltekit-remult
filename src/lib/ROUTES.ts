59142efe558caacb766dd74983c51e53c5f71cfa
Nipodemos <nipodemos13@gmail.com>
2025-11-09 21:23:40 -0300

    arquivo gerado

--- src/lib/ROUTES.ts
+++ src/lib/ROUTES.ts
@@ -15,6 +15,9 @@ const PAGES = {
   "/app/fornecedores": `/app/fornecedores`,
   "/app/fornecedores/criar_editar": `/app/fornecedores/criar_editar`,
   "/app/permissoes_telas": `/app/permissoes_telas`,
+  "/app/permissoes_telas/editar/[tela]": (params: { tela: (string | number) }) => {
+    return `/app/permissoes_telas/editar/${params['tela']}`
+  },
   "/app/produtos": `/app/produtos`,
   "/app/produtos/criar_editar": `/app/produtos/criar_editar`,
   "/app/usuarios": `/app/usuarios`,
@@ -34,6 +37,9 @@ const SERVERS = {
  */
 const ACTIONS = {
   "logout /app": `/app?/logout`,
+  "salvar /app/permissoes_telas/editar/[tela]": (params: { tela: (string | number) }) => {
+    return `/app/permissoes_telas/editar/${params['tela']}?/salvar`
+  },
   "save /app/usuarios/criar_editar": `/app/usuarios/criar_editar?/save`,
   "default /login": `/login`
 }
@@ -150,9 +156,9 @@ export function route<T extends keyof AllTypes>(key: T, ...params: any[]): strin
 * ```
 */
 export type KIT_ROUTES = {
-  PAGES: { '/app': never, '/app/categorias': never, '/app/categorias/criar_editar': never, '/app/fornecedores': never, '/app/fornecedores/criar_editar': never, '/app/permissoes_telas': never, '/app/produtos': never, '/app/produtos/criar_editar': never, '/app/usuarios': never, '/app/usuarios/criar_editar': never, '/login': never }
+  PAGES: { '/app': never, '/app/categorias': never, '/app/categorias/criar_editar': never, '/app/fornecedores': never, '/app/fornecedores/criar_editar': never, '/app/permissoes_telas': never, '/app/permissoes_telas/editar/[tela]': 'tela', '/app/produtos': never, '/app/produtos/criar_editar': never, '/app/usuarios': never, '/app/usuarios/criar_editar': never, '/login': never }
   SERVERS: Record<string, never>
-  ACTIONS: { 'logout /app': never, 'save /app/usuarios/criar_editar': never, 'default /login': never }
+  ACTIONS: { 'logout /app': never, 'salvar /app/permissoes_telas/editar/[tela]': 'tela', 'save /app/usuarios/criar_editar': never, 'default /login': never }
   LINKS: Record<string, never>
-  Params: Record<string, never>
+  Params: { 'tela': never }
 }
