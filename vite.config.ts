38fff8ddf5288fb0bbc3ffb17bdec4b179d1ff9b
Nipodemos <nipodemos13@gmail.com>
2025-11-14 15:40:54 -0300

    instala tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,19 @@
+import tailwindcss from "@tailwindcss/vite";
 import type { KIT_ROUTES } from "$lib/ROUTES";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 import { kitRoutes } from "vite-plugin-kit-routes";
 
 export default defineConfig({
-  plugins: [sveltekit(), kitRoutes<KIT_ROUTES>()],
+  plugins: [
+    tailwindcss(),
+    sveltekit(),
+    kitRoutes<KIT_ROUTES>()
+  ],
   test: {
     reporters: ["verbose"],
     include: ["src/**/*.{test,spec}.{js,ts}"],
     environment: "node",
-    setupFiles: ["./src/test-setup.ts"],
-  },
+    setupFiles: ["./src/test-setup.ts"]
+  }
 });
