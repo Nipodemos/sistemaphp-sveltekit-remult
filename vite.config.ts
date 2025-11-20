d20a2b5f1979214ce3ff474937b4032111832621
Nipodemos <nipodemos13@gmail.com>
2025-11-20 12:28:43 -0300

    adiciona devtools que o chrome pediu

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import devtoolsJson from "vite-plugin-devtools-json";
 import tailwindcss from "@tailwindcss/vite";
 import type { KIT_ROUTES } from "$lib/ROUTES";
 import { sveltekit } from "@sveltejs/kit/vite";
@@ -8,7 +9,8 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
     sveltekit(),
-    kitRoutes<KIT_ROUTES>()
+    kitRoutes<KIT_ROUTES>(),
+    devtoolsJson()
   ],
   test: {
     reporters: ["verbose"],
